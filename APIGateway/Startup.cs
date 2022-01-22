0995814baa4ff4477f355a07ddc999880330f4b7
burhan-hasan <burhan.hasan.dev@gmail.com>
2022-01-22 22:06:03 +0300

    Some changes

--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -29,13 +29,12 @@ namespace APIGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.Use(async (context, next) =>
             {
                 if (string.IsNullOrEmpty(context.Request.Path.Value.Trim('/')))
@@ -44,7 +43,7 @@ namespace APIGateway
                 }
                 await next();
             });
-            app.UseOcelot().Wait();
+            await app.UseOcelot();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
         }
