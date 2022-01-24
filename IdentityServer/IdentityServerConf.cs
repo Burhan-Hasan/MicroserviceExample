e60b498faa7511a78846b769d17bcf485ae327f5
burhan-hasan <burhan.hasan.dev@gmail.com>
2022-01-24 15:28:43 +0300

    -

---
+++ IdentityServer/IdentityServerConf.cs
@@ -0,0 +1,37 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class IdentityServerConf
+    {
+        public static IEnumerable<Client> GetClients() =>
+            new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId =  "client_id",
+                    ClientSecrets ={ new Secret("client_secrets".ToSha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { 
+                        "ORDERS_API"
+                    }
+                }
+            };
+
+        public static IEnumerable<ApiResource> GetApiResources() =>
+            new List<ApiResource>()
+            {
+                new ApiResource("ORDERS_API")
+            };
+
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() =>
+            new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId()
+            };
+    }
+}
--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,20 +1,15 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace APIGateway
+namespace IdentityServer
 {
     public class Startup
     {
@@ -28,28 +23,26 @@ namespace APIGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot(Configuration);
+            services.AddControllersWithViews();
+            services.AddIdentityServer()
+                .AddInMemoryClients(IdentityServerConf.GetClients())
+                .AddInMemoryApiResources(IdentityServerConf.GetApiResources())
+                .AddInMemoryIdentityResources(IdentityServerConf.GetIdentityResources())
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.Use(async (context, next) =>
-            {
-                if (string.IsNullOrEmpty(context.Request.Path.Value.Trim('/')))
-                {
-                    context.Request.Path = "/index.html";
-                }
-                await next();
-            });
-            app.UseAuthentication();
-            await app.UseOcelot();
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
